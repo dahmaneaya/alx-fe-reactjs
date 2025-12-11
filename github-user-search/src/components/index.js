4775a32171f094c9b480d12cc7f6cdc02bb60602
Aya <166346312+dahmaneaya@users.noreply.github.com>
2025-12-11 19:43:53 +0100

    Delete github-user-search directory

--- github-user-search/src/components/index.js
+++
@@ -1,4 +0,0 @@
-// Export all components from a central location
-export { default as Search } from './Search';
-export { default as UserCard } from './UserCard';
-export { default as SearchResults } from './SearchResults';
